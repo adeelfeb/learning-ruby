7deacbccd779128a2a6922b0b95ea6ec956a9fb1
adeelfeb <at4563323@gmail.com>
2025-07-10 17:22:05 +0500

    Update cache and server PID files - Updated binary files in the bootsnap compile cache, indicating changes in the cached bytecode. - Changed the server PID from 5517 to 6593, reflecting a new server process.

--- 2-rails/appname/app/controllers/public_controller.rb
+++ 2-rails/appname/app/controllers/public_controller.rb
@@ -4,4 +4,20 @@ class PublicController < ApplicationController
       @posts = Post.all
     end
 
+    def about
+      # Logic for about page
+    end
+
+    def contact
+      # Logic for contact page
+    end
+
+    def blog
+      @posts = Post.all
+    end 
+
+    def show
+      @post = Post.find(params[:id])
+    end 
+
 end
--- 2-rails/appname/config/routes.rb
+++ 2-rails/appname/config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+
   
+  get "about" => "public#about", as: :about
+  get "about", controller: "public", action: "about", as: :about_alternative # Alternative route for the same action
+  get  "contact" => "public#contact", as: :contact
+  get "blog" => "public#blog", as: :blog
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
